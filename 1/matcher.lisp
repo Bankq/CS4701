253a9d082d56c96a4fbad7e983505e6573236559
Bank Qian <ibank.qh@gmail.com>
2012-10-01 23:00:06 -0400

    Refactored and add tests

--- 1/matcher.lisp
+++ 1/matcher.lisp
@@ -1,28 +1,20 @@
-;;=================================================
-;;=================================================
-
 (defun match (pat exp)
-  "Top function:
-   The matcher should return bindings as follows:
-   (match '(elephant (color =c) (size =s)) 
-          '(elephant (color grey) (size 12)))"
-  (let ((binding-list (make-array 100 :fill-pointer 0 :adjustable t)))
+  (let ((binding-list (make-array 10 :fill-pointer 0 :adjustable t)))
     (catch 'fail
       (trial (list-match pat exp binding-list) binding-list))))
 
 (defun trial (test bdl)
-  (if (not test) (throw 'fail nil)
+  (if (not test) 
+      (throw 'fail nil)
       (to-list bdl)))
 
 (defun atom-match (pat exp bdl)
-  "Match two atom var.
-   if matched add to the binding list"
   (cond ((not (atom pat)) nil)
-        ((is-pattern-equal pat) (equal-pattern-match pat exp bdl))
-        ((is-pattern-not-equal pat) (not-equal-pattern-match pat exp bdl))
-        ((is-pattern-greater pat) (greater-pattern-match pat exp bdl))
-        ((is-pattern-less pat) (less-pattern-match pat exp bdl))
-        (t (plain-match pat exp))))
+        ((is-pattern pat #\=) (equal-pattern-match pat exp bdl))
+        ((is-pattern pat #\!) (not-equal-pattern-match pat exp bdl))
+        ((is-pattern pat #\>) (greater-pattern-match pat exp bdl))
+        ((is-pattern pat #\<) (less-pattern-match pat exp bdl))
+        (t (equal pat exp))))
 
 (defun list-match (pat exp bdl)
   (cond ((null pat) (if (null exp) t nil))
@@ -34,69 +26,23 @@
               always (list-match pat-item exp-item bdl)))))
 
 (defun is-pattern-any (pat)
-  "Check if a pattern start with a '&' "
-  (if (equal "&" (string (car pat)))
-      t
-      nil))
-
-(defun is-pattern-equal (pat)
-  "Check if a pattern start with a ="
-  (cond ((symbolp pat)
-         (if (equal #\= (char (string pat) 0))
-             t
-             nil))
-        (t nil)))
-
-(defun is-pattern-not-equal (pat)
-  "Check if a pattern start with a !"
-  (cond ((symbolp pat)
-         (if (equal #\! (char (string pat) 0))
-             t
-             nil))
-        (t nil)))
-
-(defun is-pattern-greater (pat)
-  "Check if a pattern start with a >"
-  (cond ((symbolp pat)
-         (if (equal #\> (char (string pat) 0))
-             t
-             nil))
-        (t nil)))
-
-(defun is-pattern-less (pat)
-  (cond ((symbolp pat)
-         (if (equal #\< (char (string pat) 0))
-             t
-             nil))
-        (t nil)))
-
+  (equal "&" (string (car pat))))
+  
 (defun is-pattern (pat pre)
-  (cond ((symbolp pat)
-         (if (equal pre (char (string pat) 0))
-             t
-             nil))
-        (t nil)))
-
-(defun plain-match (pat exp)
-  "match symbol name"
-  (equal pat exp))
+  (and (symbolp pat) (equal pre (char (string pat) 0))))
   
 (defun any-pattern-match (pat exp bdl)
   (let ((rules (cdr pat)))
     (if (null rules) t ;;(&) can match any thing
         (loop for rule in rules
              always (atom-match rule exp bdl)))))
-             
 
 (defun equal-pattern-match (pat exp bdl)
-  "pattern looks like '=a'"
   (cond ((is-bind-exist pat bdl)
          (equal (find-binding pat bdl) exp))
-        (t (insert pat exp bdl)
-           t)))
+        (t (insert pat exp bdl))))
 
 (defun not-equal-pattern-match (pat exp bdl)
-  "pattern looks like '!x'"
   (cond ((not (is-bind-exist (fmt pat) bdl)) nil)
         (t (equal exp (find-binding (fmt pat) bdl)))))
 
@@ -120,11 +66,10 @@
  (cdr (find pat bdl :key #'first)))
 
 (defun fmt (pat)
-  "format pattern !name(>name <name) to =name"
   (intern (concatenate 'string "=" (string-upcase (subseq (string pat) 1)))))
 
 (defun insert (pat exp bdl)
-  (vector-push (cons pat exp) bdl))
+  (vector-push-extend (cons pat exp) bdl) t)
 
 (defun to-list (a)
   (let ((l '())) 
@@ -132,7 +77,17 @@
     (if (equal 0 (length l)) t l)))
 
 
-
+(defun match-test ()
+  (let ((test-results 
+         '((match '(elephant (color =c) (size =s)) '(elephant (color grey) (size 12)))
+           (match '() '())
+           (match '(=1) '(1))
+           (match '(=a =b !b (& >a <b)) '(1 3 4 2))
+           (match '(&) '(a b c))
+           (not (match '(=a !a) '(1 1)))
+           )))
+    (loop for i in test-results
+         always i)))
 
 
 
